ec77d1da28ea5a91e2cbfba06a8d390e038a4565
Ashish Patel <a.patel7734@gmail.com>
2013-09-18 12:03:33 -0700

    Adding save todo list support.

--- SimpleToDo/src/com/avgtechie/simpletodo/MainActivity.java
+++ SimpleToDo/src/com/avgtechie/simpletodo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.avgtechie.simpletodo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,8 +20,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
-	ArrayList<String> items;
+	private String TAG = "MainActivity.java";
+	List<String> items;
 	ArrayAdapter<String> itemsAdapter;
 	ListView lvToDo;
 	Button btnAddItem;
@@ -35,6 +41,7 @@ public class MainActivity extends Activity {
 			public boolean onItemLongClick(AdapterView<?> aView, View item, int position, long id) {
 				items.remove(position);
 				itemsAdapter.notifyDataSetInvalidated();
+				saveToDoItems();
 				return true;
 			}
 		});
@@ -48,12 +55,15 @@ public class MainActivity extends Activity {
 		btnAddItem = (Button) findViewById(R.id.btnAddItem);
 		etNewItem = (EditText) findViewById(R.id.etAddNewItem);
 		items = new ArrayList<String>();
+		readToDoItems();
 		itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 		lvToDo.setAdapter(itemsAdapter);
 	}
 
 	public void AddToItemsList(View v) {
 		itemsAdapter.add(etNewItem.getText().toString());
+		// Log.d(TAG, items.toString());
+		saveToDoItems();
 		etNewItem.setText("");
 	}
 
@@ -62,6 +72,29 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private void saveToDoItems() {
+		File toDoFile = getToDoFile();
+		try {
+			FileUtils.writeLines(toDoFile, items);
+		} catch (IOException e) {
+			Log.e(TAG, "Error was thrown while writing file to todoFile" + e.getMessage());
+		}
+
+	}
+
+	private void readToDoItems() {
+		File toDoFile = getToDoFile();
+		try {
+			items = FileUtils.readLines(toDoFile);
+		} catch (IOException e) {
+			Log.e(TAG, "Error was thrown while writing file to todoFile" + e.getMessage());
+		}
+	}
+
+	private File getToDoFile() {
+		return new File(getFilesDir(), "todo.txt");
 	}
 
 }
